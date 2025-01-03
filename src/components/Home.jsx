a5f2a14d1ae275331b2727603a2a8aab24f2b7e2
Tarun Kumar Potti <127747782+Tarun0204@users.noreply.github.com>
2025-01-03 23:31:09 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -202,6 +202,9 @@ const Home = () => {
         </ContentContainer>
       </HomeContainer>
       <>
+        <>
+        <About />
+        </>
         <Skills />
         <Contact />
       </>
