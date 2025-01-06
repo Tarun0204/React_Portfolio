05d2bb3fe049f250c0c4fc955916d1ae837a714d
Tarun Kumar Potti <127747782+Tarun0204@users.noreply.github.com>
2025-01-06 19:14:18 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,11 @@
-import { Route, Routes, useLocation } from 'react-router-dom';
-import { useEffect } from 'react';
+import { Route, Routes} from 'react-router-dom';
 import Home from './components/Home';
 import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 
-const App = () => {
-  const location = useLocation();
-
-  useEffect(() => {
-    console.log('Route changed:', location.pathname);
-  }, [location]);
-
-  return (
+const App = () => (
     <div className="portfolio">
       <Navbar />
       <Routes>
@@ -24,6 +16,5 @@ const App = () => {
       <Footer />
     </div>
   );
-};
 
 export default App;
