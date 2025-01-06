4c84f40d42cd1f17f7ccd94a741d1128dd4ac779
Tarun Kumar Potti <127747782+Tarun0204@users.noreply.github.com>
2025-01-06 19:11:33 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
-import { Route, Routes } from 'react-router-dom';
-import Home from "./components/Home";
-import Navbar from "./components/Navbar";
-import Projects from "./components/Projects";
-import Footer from "./components/Footer"
-import Experience from "./components/Experience";
+import { Route, Routes, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
+import Home from './components/Home';
+import Experience from './components/Experience';
+import Projects from './components/Projects';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
 
-const App = () => (
-  <div className="portfolio">
-    <Navbar />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/experience" element={<Experience />} />
-      <Route path="/projects" element={<Projects />} />
-    </Routes>
-    <Footer />
-  </div>
-);
+const App = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    console.log('Route changed:', location.pathname);
+  }, [location]);
+
+  return (
+    <div className="portfolio">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/experience" element={<Experience />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
