c2e83d8e4f3cdb22882edc968ea643d4b7e82d4b
Tarun Kumar Potti <127747782+Tarun0204@users.noreply.github.com>
2025-01-06 19:15:19 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import { Route, Routes} from 'react-router-dom';
-import Home from './components/Home';
-import Experience from './components/Experience';
-import Projects from './components/Projects';
-import Footer from './components/Footer';
-import Navbar from './components/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Footer from "./components/Footer"
+import Experience from "./components/Experience";
 
 const App = () => (
-    <div className="portfolio">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/experience" element={<Experience />} />
-        <Route path="/projects" element={<Projects />} />
-      </Routes>
-      <Footer />
-    </div>
-  );
+  <div className="portfolio">
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/experience" element={<Experience />} />
+      <Route path="/projects" element={<Projects />} />
+    </Routes>
+    <Footer />
+  </div>
+);
 
 export default App;
