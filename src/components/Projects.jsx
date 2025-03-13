770ad13abcb467a8d2f5b90d15e010fc6e41dcb1
Tarun Kumar Potti <127747782+Tarun0204@users.noreply.github.com>
2025-03-13 12:04:18 +0530

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -212,7 +212,7 @@ const Projects = () => {
       : projectsData.filter((project) => project.category === activeTab);
 
   return (
-    <ProjectsApp>
+    <ProjectsApp style={{ width: "100%", overflow: "hidden" }}>
       <ProjectsHeaderContent>
         <ProjectsMainHeading>Projects</ProjectsMainHeading>
         <ProjectsPara>Explore my Projects</ProjectsPara>
